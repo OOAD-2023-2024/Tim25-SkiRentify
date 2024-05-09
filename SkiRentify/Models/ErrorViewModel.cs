bcd04e8f692054b2f6c762e24b0f30b21d1a3248
Mina Duranovic <119140559+minaduranovic@users.noreply.github.com>
2024-05-09 12:42:59 +0200

    Delete SkiRentify directory

--- SkiRentify/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SkiRentify.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
