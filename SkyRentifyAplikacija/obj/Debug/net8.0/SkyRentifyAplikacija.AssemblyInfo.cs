b7d2f7cd15712448f1eddb64d8455bfd2e99e207
kazaz03 <akazazovic1@etf.unsa.ba>
2024-06-07 15:07:17 +0200

    k

--- SkyRentifyAplikacija/obj/Debug/net8.0/SkyRentifyAplikacija.AssemblyInfo.cs
+++ SkyRentifyAplikacija/obj/Debug/net8.0/SkyRentifyAplikacija.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkyRentifyAplikacija")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15f50c0bde8e7e095f20d40c3aec7f9930cb9309")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79f712f97649af0276cd303afccaf7bc378b8253")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkyRentifyAplikacija")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkyRentifyAplikacija")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
